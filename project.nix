8ae427da5f4f9451df35b5bbe0fcb8e54135827c
Allen Nelson <anelson@narrativescience.com>
2015-12-22 17:40:32 -0600

    initial tests

--- project.nix
+++ project.nix
@@ -1,10 +1,12 @@
 { mkDerivation, base, classy-prelude, containers, stdenv, text
 , unordered-containers, cabal-install, data-fix, system-filepath
+, hspec
 }:
 mkDerivation {
   pname = "nix-eval";
   version = "0.0.0";
   src = ./.;
+  testDepends = [hspec];
   buildDepends = [
     base classy-prelude containers text unordered-containers cabal-install
     data-fix system-filepath
