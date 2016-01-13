78115db5d5af90df184d474a15b3ef5993490017
Allen Nelson <anelson@narrativescience.com>
2016-01-13 01:29:47 -0600

    added scaffold for expected builtins

--- tests/Nix/Eval/BuiltinsSpec.hs
+++ tests/Nix/Eval/BuiltinsSpec.hs
@@ -9,6 +9,7 @@ import Nix.Eval.Values
 import Nix.Eval.Values.Builtins
 import Nix.Eval.Values.NativeConversion
 import Nix.Eval.TestLib
+import Nix.Eval.ExpectedBuiltins
 
 main :: IO ()
 main = hspec spec
@@ -21,6 +22,8 @@ spec = do
   deepSeqSpec
   headSpec
   elemAtSpec
+  omapM_ describeTopLevel topLevelKeys
+  omapM_ describeBuiltinKey keysInBuiltins
 
 constantToEnvStringSpec :: Spec
 constantToEnvStringSpec = describe "constantToEnvString" $ do
@@ -91,3 +94,77 @@ elemAtSpec = describe "list index" $ do
     property $ \(i::Int) -> do
       let elem = builtin_elemAt (listV []) (fromInt i)
       elem `shouldBeErrorWith` ["IndexError"]
+
+-- | Defines a bunch of tests for top-level builtin functions.
+describeTopLevel :: Text -> Spec
+describeTopLevel name = case name of
+  "map" -> wrapDescribe mapSpec
+  name -> it "isn't written yet" $ do
+    pendingWith $ "No tests yet defined for " <> show name
+  where
+    wrapDescribe = describe ("top-level builtin " <> show name)
+    mapSpec = do
+      it "should map a function over a list" $ property $ \list ->
+        "map" @@ ("x" --> "x") @@ fromConstants list
+          `shouldEvalTo` fromConstants list
+      it "should map the +1 function over a list" $ property $ \nums -> do
+          "map" @@ ("x" --> "x" + 1) @@ (fromConstants $ map Int nums)
+            `shouldEvalTo` fromConstants (map (\i -> Int (i + 1)) nums)
+      it "should map over a nested list" $ property $ \nums1 nums2 -> do
+        let list1 = fromConstants $ map Int nums1
+            list2 = fromConstants $ map Int nums2
+            list3 = listE [list1, list2]
+        -- The ID function over a nested list
+        "map" @@ ("x" --> "x") @@ list3
+          `shouldEvalTo` listV (map (fromConstants . map Int) [nums1, nums2])
+        -- Mapping the map function
+        "map" @@ ("map" @@ ("x" --> "x")) @@ list3
+          `shouldEvalTo` listV [fromConstants $ map Int nums1,
+                                fromConstants $ map Int nums2]
+        -- Mapping functions which do things
+        "map" @@ ("map" @@ ("x" --> "x" * 2)) @@ list3
+          `shouldEvalTo` listV [fromConstants $ map (Int . (*2)) nums1,
+                                fromConstants $ map (Int . (*2)) nums2]
+
+-- | Defines a bunch of tests for keys appearing in the builtins
+-- object.
+describeBuiltinKey :: Text -> Spec
+describeBuiltinKey name = case name of
+  "length" -> wrapDescribe lengthSpec
+  "add" -> wrapSingle $ property $ \i j ->
+    bi "add" @@ intE i @@ intE j `shouldEvalTo` intV (i + j)
+  "sub" -> wrapSingle $ property $ \i j ->
+    bi "sub" @@ intE i @@ intE j `shouldEvalTo` intV (i - j)
+  "mult" -> wrapSingle $ property $ \i j ->
+    bi "mult" @@ intE i @@ intE j `shouldEvalTo` intV (i * j)
+  "div" -> wrapSingle $ property $ \i j -> do
+    case j of
+      0 -> bi "div" @@ intE i @@ intE j `shouldErrorWith` ["DivideByZero"]
+      _ -> bi "div" @@ intE i @@ intE j `shouldEvalTo` intV (i `div` j)
+  "seq" -> wrapDescribe $ do
+    it "should fail if first argument is error" $ do
+      property $ \expr ->
+        shouldError $ bi "seq" @@ failingExpression @@ expr
+    it "should return second argument if first argument succeeds" $ do
+      property $ \constant -> do
+        let expr = fromConstant constant
+        res <- evalStrict expr
+        seqRes <- evalStrict (bi "seq" @@ succeedingExpression @@ expr)
+        res `shouldBe` seqRes
+  "deepSeq" -> deepSeqSpec
+  "elemAt" -> elemAtSpec
+  "head" -> headSpec
+  name -> it "isn't written yet" $ do
+    pendingWith $ "No tests yet defined for " <> show name
+  where
+    bi key = "builtins" !. key
+    wrapDescribe = describe ("builtin key " <> show name)
+    wrapSingle test = wrapDescribe $ it "should work" $ test
+    lengthSpec = do
+      it "should get the length of a list" $ property $ \list ->
+        bi "length" @@ fromConstants list
+          `shouldEvalTo` fromInt (length list)
+      it "shouldn't matter if the list has an error value" $ do
+        property $ \list -> do
+          bi "length" @@ (listE (failingExpression : map fromConstant list))
+            `shouldEvalTo` fromInt (length list + 1)
